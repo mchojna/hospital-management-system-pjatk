8a46afc8e9fba93af4ee25aceffe5366d0e6b973
Michal Chojna <m.chojna@icloud.com>
2026-05-31 21:35:33 +0200

    Update exception

--- HospitalManagementSystem/Exceptions/NotFoundException.cs
+++ HospitalManagementSystem/Exceptions/NotFoundException.cs
@@ -2,5 +2,7 @@ namespace HospitalManagementSystem.Exceptions;
 
 public class NotFoundException : Exception
 {
-    public NotFoundException(string message) : base(message) { }
+    public NotFoundException(string message) : base(message)
+    {
+    }
 }
