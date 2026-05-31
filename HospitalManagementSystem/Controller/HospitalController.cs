2f1638f892956b7fec2c8fa33832ad9c76e8951a
Michal Chojna <m.chojna@icloud.com>
2026-05-31 20:58:51 +0200

    Create controller

---
+++ HospitalManagementSystem/Controller/HospitalController.cs
@@ -0,0 +1,42 @@
+using HospitalManagementSystem.DTOs;
+using HospitalManagementSystem.Exceptions;
+using HospitalManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.Controller;
+
+[Route("api/patients")]
+[ApiController]
+public class HospitalController(IDbService service) : ControllerBase
+{
+    private readonly IDbService _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> GetPatients([FromQuery] string? search)
+    {
+        try
+        {
+            var result = await _service.GetPatientsAsync(search);
+            return Ok(result);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [Route("{pesel}/bedassignments")]
+    [HttpPost]
+    public async Task<IActionResult> AssignPatientToBed([FromRoute] string pesel, [FromBody] AssignBedDto assignBedDto)
+    {
+        try
+        {
+            await _service.AssignPatientToBed(pesel, assignBedDto);
+            return Created();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound();
+        }
+    }
+}
